425f7561ef05c8f6b6a21dda54640abb75108a7a
Shopping <junhyng102@gmail.com>
2023-12-12 23:14:50 +0900

    Calendar Practice Project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalenderPracticeProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void lblNote_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtNote.Focus();
+        }
+
+        private void txtNote_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNote.Text) && txtNote.Text.Length > 0)
+            {
+                lblNote.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lblNote.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void lblTime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtTime.Focus();
+        }
+
+        private void txtTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTime.Text) && txtTime.Text.Length > 0)
+            {
+                lblTime.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lblTime.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ UserControls/MenuButton.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalenderPracticeProject.UserControls
+{
+    /// <summary>
+    /// MenuButton.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        //Caption속성을 위한 종속속성 CaptionProperty 등록
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.
+            Register("Caption", typeof(string), typeof(MenuButton));
+
+        public FontAwesome.WPF.FontAwesomeIcon Icon
+        {
+            get { return (FontAwesome.WPF.FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        //Icon속성을 위한 종속속성 IconProperty 등록
+        public static readonly DependencyProperty IconProperty = DependencyProperty.
+            Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(MenuButton));
+    }
+}
